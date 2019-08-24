b601388ae4122da3471ea2313e9e54ed04ed7820
NagaoKyota <kyota.n.2014@gmail.com>
2019-08-24 15:10:51 +0900

    add DisplayCode component

---
+++ src/components/DisplayCode.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Blockly from 'node-blockly/browser';
+
+export default class DisplayCode extends Component {
+  render() {
+    return (
+      <div>
+        hoge
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Col, Row } from 'react-bootstrap';
 import BlocklyComponent from './components/BlocklyComponent';
+import DisplayCode from './components/DisplayCode';
 
 const App = () => (
-  <BlocklyComponent />
+  <div>
+    <Container fluid style={{ marginTop: `10px` }}>
+      <Row>
+        <Col md={7}>
+          <BlocklyComponent />
+        </Col>
+        <Col md={5}>
+          <DisplayCode />
+        </Col>
+      </Row>
+    </Container>
+  </div>
 );
 
 ReactDOM.render(<App />, window.document.getElementById('root'));
