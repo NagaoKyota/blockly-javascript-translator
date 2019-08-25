267074ee32fc6fca5c92af17a5b595e07d2ad3ce
NagaoKyota <kyota.n.2014@gmail.com>
2019-08-25 13:51:41 +0900

    setting jsBeautifier

--- src/components/DisplayCode.js
+++ src/components/DisplayCode.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import * as jsBeautify from 'js-beautify';
+
+const beautifyOptions = {
+  indent_size: "2",
+  indent_char: " ",
+  max_preserve_newlines: "-1",
+  indent_scripts: "keep",
+  brace_style: "end-expand",
+  space_before_conditional: true,
+  wrap_line_length: "40",
+};
 
 class DisplayCode extends Component {
   render() {
     return (
       <div>
-        <pre>{this.props.store.blocklyCode}</pre>
+        <pre>{jsBeautify.js(this.props.store.blocklyCode, beautifyOptions)}</pre>
       </div>
     );
   }
