4cbc05a4889bfa54f800dfa216a495746feff34f
NagaoKyota <kyota.n.2014@gmail.com>
2019-08-27 07:32:23 +0900

    modify library import

--- src/components/WriteCode.js
+++ src/components/WriteCode.js
@@ -1,16 +1,5 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import * as jsBeautify from 'js-beautify';
-
-const beautifyOptions = {
-  indent_size: "2",
-  indent_char: " ",
-  max_preserve_newlines: "-1",
-  indent_scripts: "keep",
-  brace_style: "end-expand",
-  space_before_conditional: true,
-  wrap_line_length: "0",
-};
 
 class WriteCode extends Component {
   handleOnChange(e) {
